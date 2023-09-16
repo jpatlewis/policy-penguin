da3fb4974391615de47ed48c654c6d8c44455040
jon_wsl <jpatlewis@hotmail.com>
2023-09-15 20:10:23 -0400

    all capsy

--- src/config/config.py
+++ src/config/config.py
@@ -3,4 +3,4 @@ from configparser import ConfigParser
 config = ConfigParser()
 config.read('config.cfg')
 
-gpt_api_key = config['local']['GPT_API_KEY']
+GPT_API_KEY = config['local']['GPT_API_KEY']
